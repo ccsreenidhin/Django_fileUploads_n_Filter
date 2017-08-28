6f22ad011bea15a8aed9f7f842412daccf47daf6
c c sreenidhin <ccsreenidhin@gmail.com>
2017-08-28 23:36:58 +0530

    initial commit

---
+++ Django_multiple-file-upload_from_SimpleIsbetterthancomplex/mysite/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^photos/', include('mysite.photos.urls', namespace='photos')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
